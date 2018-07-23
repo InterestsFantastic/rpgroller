a5673a4f0167a4c631f46c85a8154decbcf1f081
DavidCaughell <30320765+DavidCaughell@users.noreply.github.com>
2018-07-22 21:50:26 -0400

    Update roller.py
    Added rerolling low dice, including rerolling unlimited times or once.

--- roller.py
+++ roller.py
@@ -1,3 +1,8 @@
+# then 5d10+1>8 ... roll 5d10 +1 TO EACH DIE! and check how many successes vs 8. So you should check for the > before you check for the + etc.
+# then 5d10+1>8s ... this will count as a specialized roll for v:tm (i.e. 10 counts as two successes)
+# do the verbose so that I can see everything going on in the rolls... 3d6+1 should show the original and final roll, and the
+# difficulty check should show each adjusted dice and mark its success or failure.
+
 #!/usr/bin/python2
 '''Rolls dice using the format you may see in a tabletop RPG, e.g. '3d6'.
 
@@ -5,11 +10,13 @@ Roll Description String Format:
     1d6         Rolls a dice with 6 sides
     d6          Same
     12d4        Sum of twelve d4 results
-    4d6kh3      Rolls 4d6 and returns the sum of the highest three dice.
+    4d6kh3      Rolls 4d6 and returns the sum of the highest three dice
     4d6kl3      ...lowest
     2d6+2       Sum of 2d6 and 2
     2d6-1       Sum of 2d6 and -1
     3d8*10      Product of 3d8 and 10
+    3d6rr2      Rolls 3d6 but rerolls any 2's or below
+    3d6rr2o     Rerolls only once per die
 
 Improvements Required:
     -ask reddit if it's necessary to protect the _PATTERN name, etc.
@@ -18,12 +25,16 @@ Improvements Required:
     -Add rerolling (reroll 1's, etc. by default reroll low but potentially also reroll high)
 
     -make sure that all kinds of stuff fails that should fail will fail in the testing area.
+
+    -convert the docstrings to markdown for github
 '''
 
 _DEBUG = False
 _TESTS = False
+_UPDATE_DOCSTRING_OUTPUT = False
 ##_DEBUG = True
 _TESTS = True
+##_UPDATE_DOCSTRING_OUTPUT = True
 
 import re, random
 random.seed()
@@ -40,7 +51,7 @@ def _tests():
     _test_rolls()
 
 def _test_rolls():
-    test = Roller('3d6')
+    test = Roller('3d6rr2o', verbose=True)
     for n in range(6):
         test.roll()
     test.newroll('3d8*10')
@@ -69,7 +80,7 @@ def _test_rollstrings():
 def _parse(rollstr):
     assert _COMPILED.match(rollstr)
     up = rollstr.upper()
-    
+
     # Number and sides of dice
     pat = re.compile(r'\d*D\d+')
     m = pat.match(up)
@@ -80,6 +91,17 @@ def _parse(rollstr):
     assert numdice > 0
     assert dicesides > 0
 
+    # Rerolling instructions
+    pat = re.compile(r'RR\d+O?')
+    m = pat.search(up)
+    once = False
+    reroll = None
+    if m:
+        m = m.group()[2:]
+        if m[-1] == 'O': once = True 
+        reroll = int(m[:-1]) if once else int(m)
+    assert reroll is None or (reroll > 0 and reroll < dicesides)
+    
     # Make function to modify results (+2, *10, etc.).
     pat = re.compile(r'[*X+-]\d+')
     m = pat.search(up)
@@ -107,7 +129,7 @@ def _parse(rollstr):
     else:
         keepdice = None
 
-    return numdice, dicesides, keepdice, modify
+    return numdice, dicesides, keepdice, modify, reroll, once
 
 
 def roll(rolldesc='1d20', min1=True, verbose=False):
@@ -118,12 +140,30 @@ def roll(rolldesc='1d20', min1=True, verbose=False):
     verbose flagged True returns the result, the individual dice rolls that made it (in the case of multiple dice), and the rolldesc.
     '''
     
-    numdice, dicesides, keepdice, modify = _parse(rolldesc)
+    numdice, dicesides, keepdice, modify, reroll, once = _parse(rolldesc)
 
-    rolls = []
+    # Because when infinite rerolls are allowed, we don't bother offering a chance of rolling too low,
+    # this gathers MOST of the data for the rolls for presentation during verbose output
+    origrolls = []
     for n in range(numdice):
-        rolls.append(random.randint(1, dicesides))
-    origrolls = list(rolls)
+        if reroll:
+            if not once:
+                origrolls.append(random.randint(reroll+1, dicesides))
+            else:
+                origrolls.append(random.randint(1, dicesides))
+                if origrolls[-1] <= reroll:
+                    origrolls[-1] = [origrolls[-1],random.randint(1, dicesides)]
+        else:
+            origrolls.append(random.randint(1, dicesides))
+
+    #define rolls here and condense it
+    rolls = []
+    for n in origrolls:
+        try:
+            rolls.append(n[1])
+        except:
+            rolls.append(n)
+    rolls = list(rolls)
     
     if keepdice:
         rolls = keepdice(rolls)
@@ -204,7 +244,7 @@ class Roller:
     def initout(self, *args):
         '''Pass through method to initialize your output object. Default is good values for OutTerm'''
         if len(args) == 0:
-            self.out = self.out(self.results)
+            self.out = self.out(self.results, verbose=self.verbose)
         else:
             self.out = self.out(*args)
 
@@ -214,3 +254,5 @@ class Roller:
 
 if __name__ == '__main__':
     if _TESTS: _tests()
+    if _UPDATE_DOCSTRING_OUTPUT:
+        pass
